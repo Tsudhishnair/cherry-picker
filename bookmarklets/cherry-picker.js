0d7fd561d43f80b104abf7d479a0eb6e7c485d70
tsudhishnair <sudhishnair53@gmail.com>
2021-01-10 16:21:38 +0530

    Window alert, showing the commits in the PR

--- bookmarklets/cherry-picker.js
+++ bookmarklets/cherry-picker.js
@@ -1,10 +1,11 @@
-(function(){
-  let all_SHA_ele = document.querySelectorAll('clipboard-copy.btn.btn-outline');
+// Code that copies gets the SHA's from the PR and shows them in the alert popup
+(function () {
+  let all_SHA_ele = document.querySelectorAll("clipboard-copy.btn.btn-outline");
   const commitShaList = [];
-  for (const element of all_SHA_ele)
-    {
-      commitShaList.push(element.getAttribute('value'));
-    }
-  alert('Hey how are uou',commitShaList);
-  console.log(commitShaList);
+  for (const element of all_SHA_ele) {
+    commitShaList.push(element.getAttribute("value"));
+  }
+  let cmd = `git cherry-pick ${commitShaList.join(" ")}`;
+  window.alert(cmd);
+  console.log(cmd);
 })();
